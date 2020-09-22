2629ae1561a60f7e2e5b8aff03e61c191a69f295
Sarah Svedenborg <sarah.svedenborg@gmail.com>
2020-09-22 21:39:17 +0200

    fixed typo in authentication action

--- src/store/actions/orders.js
+++ src/store/actions/orders.js
@@ -61,7 +61,7 @@ export const fetchOrdersStart = () => {
 export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart())
-        const queryParams ='?auth' + token + '&orderBy="userId"&equalTo="' + userId + '"'
+        const queryParams ='?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"'
         axios.get('/orders.json' + queryParams)
             .then(response => {
                 const fetchedOrders = []
